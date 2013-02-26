cc86401169ae167960e0c398533e862618a6ae09
simonjrp <simon@simon-M14xR2>
2013-02-26 17:39:21 +0100

    Fixed the layout, changed to white background and added icon to the 'buy' button.

--- src/imat/gui/ProductThumbnail.java
+++ src/imat/gui/ProductThumbnail.java
@@ -24,6 +24,11 @@ import javax.swing.BoxLayout;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import net.miginfocom.swing.MigLayout;
+import java.awt.CardLayout;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 
 public class ProductThumbnail extends JPanel {
 	private static final IMatDataHandler IDH = IMatDataHandler.getInstance();
@@ -31,14 +36,14 @@ public class ProductThumbnail extends JPanel {
 			new Integer(1), null, new Integer(1));
 	SpinnerModel kgModel = new SpinnerNumberModel(new Double(0.1), new Double(
 			0.1), null, new Double(0.1));
+	private Product product;
 	private JPanel panel;
 	private JLabel titleLabel;
-	private JLabel priceLabel;
 	private JSpinner qSpinner;
-	private JLabel sumLabel;
-	private JButton buyButton;
 	private JLabel suffixLabel;
-	private Product product;
+	private JButton buyButton;
+	private JLabel sumLabel;
+	private JLabel priceLabel;
 	private JPanel panel_1;
 	private JLabel imageLabel;
 
@@ -46,104 +51,94 @@ public class ProductThumbnail extends JPanel {
 	 * Create the panel.
 	 */
 	public ProductThumbnail(Product p, boolean featured) {
-		setBorder(new LineBorder(Color.LIGHT_GRAY));
+		setBackground(Color.WHITE);
+		
 		product = p;
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		
+		setPreferredSize(new Dimension(300, 126));
+		setBorder(new LineBorder(Color.LIGHT_GRAY));
+		setLayout(new BorderLayout(0, 0));
+		
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		add(panel, BorderLayout.CENTER);
+		
+		titleLabel = new JLabel("Label");
+		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+		
+		qSpinner = new JSpinner();
+		qSpinner.setFont(new Font("SansSerif", Font.BOLD, 12));
+		qSpinner.addChangeListener(new QSpinnerChangeListener());
+		
+		suffixLabel = new JLabel("st");
+		suffixLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		
+		buyButton = new JButton("");
+		buyButton.setMaximumSize(new Dimension(60, 30));
+		buyButton.setMinimumSize(new Dimension(60, 30));
+		buyButton.setPreferredSize(new Dimension(60, 30));
+		buyButton.setOpaque(false);
+		buyButton.setIcon(new ImageIcon(ProductThumbnail.class.getResource("/imat/resources/buyButton60x30.PNG")));
+		buyButton.setFont(new Font("SansSerif", Font.BOLD, 12));
+		
+		sumLabel = new JLabel("Sum");
+		sumLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		priceLabel = new JLabel("Desc");
+		priceLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(titleLabel)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(qSpinner, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(suffixLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(sumLabel, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(buyButton, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(priceLabel))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(buyButton, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(titleLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(priceLabel)
+							.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+								.addComponent(qSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(suffixLabel)
+								.addComponent(sumLabel))))
+					.addContainerGap())
+		);
+		panel.setLayout(gl_panel);
 		
 		panel_1 = new JPanel();
-		add(panel_1);
-		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		panel_1.setBackground(Color.WHITE);
+		add(panel_1, BorderLayout.WEST);
 		
 		imageLabel = new JLabel("");
+		imageLabel.setBorder(new LineBorder(new Color(128, 128, 128), 2));
 		panel_1.add(imageLabel);
-
-		panel = new JPanel();
-		panel.setBorder(null);
-		add(panel);
-
-		titleLabel = new JLabel("Titel");
-		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
-
-		priceLabel = new JLabel("Price");
-		priceLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
-
-		qSpinner = new JSpinner();
-
-		sumLabel = new JLabel("Sum");
-
-		buyButton = new JButton("Köp");
-
-		suffixLabel = new JLabel("Suf");
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(gl_panel
-				.createParallelGroup(Alignment.LEADING)
-				.addGroup(
-						gl_panel.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(
-										gl_panel.createParallelGroup(
-												Alignment.LEADING, false)
-												.addComponent(
-														titleLabel,
-														GroupLayout.DEFAULT_SIZE,
-														GroupLayout.DEFAULT_SIZE,
-														Short.MAX_VALUE)
-												.addGroup(
-														gl_panel.createSequentialGroup()
-																.addGroup(
-																		gl_panel.createParallelGroup(
-																				Alignment.TRAILING,
-																				false)
-																				.addComponent(
-																						qSpinner,
-																						Alignment.LEADING,
-																						GroupLayout.PREFERRED_SIZE,
-																						GroupLayout.DEFAULT_SIZE,
-																						GroupLayout.PREFERRED_SIZE)
-																				.addComponent(
-																						priceLabel,
-																						Alignment.LEADING,
-																						GroupLayout.DEFAULT_SIZE,
-																						GroupLayout.DEFAULT_SIZE,
-																						Short.MAX_VALUE))
-																.addPreferredGap(
-																		ComponentPlacement.RELATED)
-																.addComponent(
-																		sumLabel)))
-								.addPreferredGap(ComponentPlacement.UNRELATED)
-								.addComponent(suffixLabel)
-								.addPreferredGap(ComponentPlacement.RELATED,
-										GroupLayout.DEFAULT_SIZE,
-										Short.MAX_VALUE)
-								.addComponent(buyButton).addContainerGap()));
-		gl_panel.setVerticalGroup(gl_panel
-				.createParallelGroup(Alignment.LEADING)
-				.addGroup(
-						gl_panel.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(titleLabel)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(priceLabel)
-								.addPreferredGap(ComponentPlacement.RELATED,
-										34, Short.MAX_VALUE)
-								.addGroup(
-										gl_panel.createParallelGroup(
-												Alignment.BASELINE)
-												.addComponent(
-														qSpinner,
-														GroupLayout.PREFERRED_SIZE,
-														GroupLayout.DEFAULT_SIZE,
-														GroupLayout.PREFERRED_SIZE)
-												.addComponent(sumLabel)
-												.addComponent(buyButton)
-												.addComponent(suffixLabel))
-								.addContainerGap()));
-		panel.setLayout(gl_panel);
+		
+		
 		// Set labels and image according to the product
 		titleLabel.setText(p.getName());
 		priceLabel.setText(p.getPrice() + " " + p.getUnit());
+		
 		if (featured) {
-			imageLabel.setPreferredSize(new Dimension(200, 150));
+			titleLabel.setFont(new Font("SansSerif", Font.BOLD, 27));
+			priceLabel.setFont(new Font("SansSerif", Font.PLAIN, 18));
 			imageLabel.setIcon(IDH.getImageIcon(p, new Dimension(200, 150)));
 		} else {
 			imageLabel.setIcon(IDH.getImageIcon(p, new Dimension(140, 105)));
@@ -156,9 +151,6 @@ public class ProductThumbnail extends JPanel {
 			qSpinner.setModel(stModel);
 			suffixLabel.setText("st");
 		}
-		// if(featured) {
-		// this.setPreferredSize(new Dimension(844,128));
-		// }
 		updateSum();
 	}
 
@@ -173,11 +165,9 @@ public class ProductThumbnail extends JPanel {
 		DecimalFormat d = new DecimalFormat("#.00");
 		sumLabel.setText(d.format(newSum) + " kr");
 	}
-
 	private class QSpinnerChangeListener implements ChangeListener {
-		public void stateChanged(ChangeEvent e) {
+		public void stateChanged(ChangeEvent arg0) {
 			updateSum();
 		}
 	}
-
 }
