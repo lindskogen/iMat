d56256bcae34a7f2b3391cb981e7c1554900c197
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-02-27 16:57:53 +0100

    Implemented fuzzy-search functionality

--- src/imat/backend/ShopModel.java
+++ src/imat/backend/ShopModel.java
@@ -9,10 +9,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 
 import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
 import se.chalmers.ait.dat215.project.ShoppingCart;
 
 public class ShopModel {
@@ -66,5 +72,24 @@ public class ShopModel {
 			}
 		}
 	}
-
+	public List<String> fuzzySearch(String str) {
+		Map<String, Integer> keywords = new HashMap<String,Integer>();
+		int lowest = 999;
+		for (Product p : imat.getProducts()) {
+			int distance = StringUtils.getLevenshteinDistance(str.toLowerCase(), p.getName().toLowerCase(), str.length()/2 + 1);
+			if (distance != -1) {
+				keywords.put(p.getName(), distance);
+				if (distance < lowest) {
+					lowest = distance;
+				}
+			}
+		}
+		ArrayList<String> res = new ArrayList<String>();
+		for (String s : keywords.keySet()) {
+			if (keywords.get(s) == lowest) {
+				res.add(s);
+			}
+		}
+		return res;
+	}
 }
