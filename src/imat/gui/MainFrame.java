ff35f6f0adca77b8c8068b123387f849a2c8e97a
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-02-27 16:25:46 +0100

    Organized imports

---
+++ src/imat/gui/MainFrame.java
@@ -0,0 +1,50 @@
+package imat.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+public class MainFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(50, 50, 900, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		ProductsView productsView = ProductsView.getSharedInstance();
+		frame.getContentPane().add(productsView, BorderLayout.CENTER);
+
+		TabbedView tabbedView = new TabbedView();
+		frame.getContentPane().add(tabbedView, BorderLayout.EAST);
+	}
+
+}
