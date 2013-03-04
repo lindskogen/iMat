3de8ef4e18781101c3a6a8248e0e68d0cb8ac3b9
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-03-04 14:34:55 +0100

    Added generic type arguments

--- src/imat/gui/Checkout.java
+++ src/imat/gui/Checkout.java
@@ -53,10 +53,10 @@ public class Checkout implements ActionListener {
 	private JTextField txtCard;
 	private JTextField txtSec;
 	private JPanel cardPanel;
-	private JComboBox delivery;
-	private JComboBox pickup;
-	private JComboBox year;
-	private JComboBox month;
+	private JComboBox<String> delivery;
+	private JComboBox<String> pickup;
+	private JComboBox<String> year;
+	private JComboBox<String> month;
 	private JTextField txtName;
 	private JRadioButton visa;
 	private JRadioButton mastercard;
@@ -388,13 +388,13 @@ public class Checkout implements ActionListener {
 		
 		JLabel lblKorttyp = new JLabel("Korttyp");
 		
-		JComboBox comboMonth = new JComboBox();
+		JComboBox<String> comboMonth = new JComboBox<String>();
 		month = comboMonth;
-		comboMonth.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		comboMonth.setModel(new DefaultComboBoxModel<String>(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
 		
-		JComboBox comboYear = new JComboBox();
+		JComboBox<String> comboYear = new JComboBox<String>();
 		year = comboYear;
-		comboYear.setModel(new DefaultComboBoxModel(new String[] {"13", "14", "15", "16", "17", "18", "19", "20", "21"}));
+		comboYear.setModel(new DefaultComboBoxModel<String>(new String[] {"13", "14", "15", "16", "17", "18", "19", "20", "21"}));
 		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
 		gl_panel_4.setHorizontalGroup(
 			gl_panel_4.createParallelGroup(Alignment.LEADING)
@@ -462,8 +462,8 @@ public class Checkout implements ActionListener {
 		JRadioButton rdbtnHemleveransKr = new JRadioButton("Hemleverans");
 		rdbtnHemleveransKr.setSelected(true);
 		
-		JComboBox deliveryBox = new JComboBox();
-		deliveryBox.setModel(new DefaultComboBoxModel(new String[] {"Måndag em", "Tisdag fm", "Tisdag em", "Onsdag fm", "Onsdag em"}));
+		JComboBox<String> deliveryBox = new JComboBox<String>();
+		deliveryBox.setModel(new DefaultComboBoxModel<String>(new String[] {"Måndag em", "Tisdag fm", "Tisdag em", "Onsdag fm", "Onsdag em"}));
 		
 		JRadioButton rdbtnHmtaIButik = new JRadioButton("Hämta i butik");
 		
@@ -475,9 +475,9 @@ public class Checkout implements ActionListener {
 		rdbtnHmtaIButik.addActionListener(this);
 		rdbtnHmtaIButik.setActionCommand("pickup");
 		
-		JComboBox pickupBox = new JComboBox();
+		JComboBox<String> pickupBox = new JComboBox<String>();
 		pickupBox.setEnabled(false);
-		pickupBox.setModel(new DefaultComboBoxModel(new String[] {"Måndag em", "Tisdag fm", "Tisdag em", "Onsdag fm", "Onsdag em"}));
+		pickupBox.setModel(new DefaultComboBoxModel<String>(new String[] {"Måndag em", "Tisdag fm", "Tisdag em", "Onsdag fm", "Onsdag em"}));
 		
 		delivery = deliveryBox;
 		pickup = pickupBox;
